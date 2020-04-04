2209ea270819eedfe441f40cd59a3d279ee275c3
JaeYooong <rladltjq123@hanmail.net>
2020-04-04 14:31:40 +0900

    myself

--- seungjo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import session_2.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', session_2.views.index, name="index")
-]
